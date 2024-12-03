9632e796a9638d33e139c5458cdb97e54ca75322
lewenhagen <lewenhagen@live.com>
2024-12-03 15:56:11 +0100

    more ignores

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import fs from 'node:fs/promises'
 
 let course = process.argv[2]
 let url = "data/november.xlsx"
-let courses =(await fs.readFile('courseCodes', {encoding: 'utf8'})).split("\n")
+let courses =(await fs.readFile('courseCodes', {encoding: 'utf8'})).trim().split("\n")
 let result = {}
+// result["grandTotal"] = 0
+let grandTotal = 0
 
 let workbook = XLSX.readFile(url)
 // let sheet_name_list = workbook.SheetNames
@@ -13,22 +15,49 @@ let courseData = xlData.filter(function(temp) {
   return courses.includes(temp['kurskod '])
 })
 
+function roundShit (num) {
+  return  Math.round((num  * 100) / 100)
+}
+function formatNumber(x) {
+    return new Intl.NumberFormat('sv-SE', { style: 'currency', currency: 'SEK' }).format(
+      x
+    )
+}
 for (const course of courses) {
+  let obj = {
+    "HST": 0,
+    "HPR": 0,
+    "HP": 0,
+    "HST_kr_stat": 0,
+    "HPR_kr_stat": 0,
+    "studAntal": 0,
+    "total": 0
+  }
+
   result[course] = []
+
   for (const item of courseData) {
     if (item["kurskod "] === course) {
-      result[course].push(
-        {
-          "HST": item["HST "],
-          "HPR": item["HPR "],
-          "HP": item["hp "],
-          "HST_kr_stat": item["HST kr (stat) "],
-          "HPR_kr_stat": item["HPR kr (stat) "],
-          "studAntal": item["Antal stud "],
-        }
-      )
+      obj["HST"] += roundShit(parseFloat(item["HST "]))
+      obj["HPR"] += roundShit(parseFloat(item["HPR "]))
+      obj["HP"] = parseFloat(item["hp "])
+      obj["HST_kr_stat"] += roundShit(parseFloat(item["HST kr (stat) "]))
+      obj["HPR_kr_stat"] += roundShit(parseFloat(item["HPR kr (stat) "]))
+      obj["studAntal"] += parseInt(item["Antal stud "])
+      obj["total"] += roundShit(parseFloat(item["HST kr (stat) "]) + parseFloat(item["HPR kr (stat) "]))
+      obj["prest_grad"] = roundShit(parseFloat((obj["HPR"]/obj["HST"])*100)) + "%"
     }
   }
+  result[course] = obj
 }
 
-console.log(result)
+for (const course of courses) {
+  grandTotal += roundShit(result[course]["HST_kr_stat"] + result[course]["HPR_kr_stat"])
+  result[course]["HST_kr_stat"] = formatNumber(result[course]["HST_kr_stat"])
+  result[course]["HPR_kr_stat"] = formatNumber(result[course]["HPR_kr_stat"])
+  result[course]["total"] = formatNumber(result[course]["total"])
+}
+// grandTotal = formatNumber(grandTotal)
+
+console.table(result)
+console.table({"Grand Total": formatNumber(grandTotal)})
